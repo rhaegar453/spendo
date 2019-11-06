f935577e9fc06068126c1df10a2b5da06cd20500
Shivaraj Bakale <shivarajapple@Shivarajs-MacBook-Pro.local>
2019-11-06 13:52:42 +0530

    Navbar added

---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './style.css';
+import LightLogo from '../Assets/logo.svg';
+
+
+const Navbar = () => {
+    return (
+        <nav class="navbar navbar-light accent">
+            <span class="navbar-brand mb-0 h1">
+            <img src={LightLogo} alt="Logo"></img>
+            </span>
+        </nav>
+    );
+}
+
+export default Navbar;
